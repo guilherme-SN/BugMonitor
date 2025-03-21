b446f7f4f7c6a0ad68764d35f688d1c22e2b90b4
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-03-21 15:31:33 -0300

    feat: add table name, builder and transform bidirectional to unidirectional relationship

--- src/main/java/br/com/ccm/api/bugmonitor/model/User.java
+++ src/main/java/br/com/ccm/api/bugmonitor/model/User.java
@@ -2,31 +2,24 @@ package br.com.ccm.api.bugmonitor.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private UUID uuid;
+    private String uuid;
     private String email;
     private String name;
-
-    @OneToMany(mappedBy = "createdBy", cascade = CascadeType.ALL, orphanRemoval = false)
-    private Set<Bug> createdBugs = new HashSet<>();
-
-    @ManyToMany(mappedBy = "backendResponsibles", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
-    private Set<Bug> backendResponsibleBugs = new HashSet<>();
-
-    @ManyToMany(mappedBy = "frontendResponsibles", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
-    private Set<Bug> frontendResponsibleBugs = new HashSet<>();
 }
