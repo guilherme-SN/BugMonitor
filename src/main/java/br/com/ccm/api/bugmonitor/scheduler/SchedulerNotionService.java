dddc6eb3c427f6f88f3615061a07c6003db52e06
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-03-29 17:38:25 -0300

    refactor: move notion page processing logic to BugService

--- src/main/java/br/com/ccm/api/bugmonitor/scheduler/SchedulerNotionService.java
+++ src/main/java/br/com/ccm/api/bugmonitor/scheduler/SchedulerNotionService.java
@@ -1,43 +1,17 @@
 package br.com.ccm.api.bugmonitor.scheduler;
 
-import br.com.ccm.api.bugmonitor.command.notion.outputs.NotionResponse;
-import br.com.ccm.api.bugmonitor.command.notion.outputs.attribute.NotionPage;
-import br.com.ccm.api.bugmonitor.model.Bug;
-import br.com.ccm.api.bugmonitor.repository.BugRepository;
 import br.com.ccm.api.bugmonitor.service.BugService;
 import br.com.ccm.api.bugmonitor.service.NotionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class SchedulerNotionService {
-    private final BugRepository bugRepository;
     private final NotionService notionService;
     private final BugService bugService;
 
     public void retrieveNotionDatabase() {
-        notionService.getDatabase().subscribe(this::processNotionDatabase);
-    }
-
-    private void processNotionDatabase(NotionResponse notionResponse) {
-        for (NotionPage notionPage : notionResponse.notionPages()) {
-            processNotionPage(notionPage);
-        }
-    }
-
-    private void processNotionPage(NotionPage notionPage) {
-        Optional<Bug> bugOptional = bugRepository.findById(notionPage.properties().pageId().uniqueId().number());
-
-        if (bugOptional.isEmpty()) {
-            bugService.saveFromNotionPage(notionPage);
-        } else {
-            checkForUpdatesOnPage(notionPage, bugOptional.get());
-        }
-    }
-
-    private void checkForUpdatesOnPage(NotionPage notionPage, Bug savedBug) {
+        notionService.getDatabase().subscribe(bugService::processNotionDatabase);
     }
 }
--- src/main/java/br/com/ccm/api/bugmonitor/service/BugService.java
+++ src/main/java/br/com/ccm/api/bugmonitor/service/BugService.java
@@ -1,11 +1,14 @@
 package br.com.ccm.api.bugmonitor.service;
 
+import br.com.ccm.api.bugmonitor.command.notion.outputs.NotionResponse;
 import br.com.ccm.api.bugmonitor.command.notion.outputs.attribute.NotionPage;
 import br.com.ccm.api.bugmonitor.model.Bug;
 import br.com.ccm.api.bugmonitor.repository.BugRepository;
 import br.com.ccm.api.bugmonitor.util.NotionPageExtractor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +17,22 @@ public class BugService {
     private final DiscordNotificationService discordNotificationService;
     private final BugRepository bugRepository;
 
-    public void saveFromNotionPage(NotionPage notionPage) {
+    public void processNotionDatabase(NotionResponse notionResponse) {
+        notionResponse.notionPages().forEach(this::processNotionPage);
+    }
+
+    private void processNotionPage(NotionPage notionPage) {
+        Optional<Bug> bugOptional = bugRepository.findById(notionPage.properties().pageId().uniqueId().number());
+
+        if (bugOptional.isEmpty()) {
+            createBugFromNotionPage(notionPage);
+        } else {
+            updateBugIfNeeded(notionPage, bugOptional.get());
+        }
+    }
+
+    public void createBugFromNotionPage(NotionPage notionPage) {
         Bug bug = notionPageExtractor.extractBugFromNotionPage(notionPage);
-        discordService.sendNewBugNotification(bug);
         bugRepository.save(bug);
         discordNotificationService.notifyNewBug(bug);
     }
