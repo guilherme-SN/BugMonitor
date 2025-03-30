397142ea533ec6d89e2d833f3d53836231bbff7e
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-03-30 11:33:35 -0300

    feat: add @Setter to Bug entity

--- src/main/java/br/com/ccm/api/bugmonitor/model/Bug.java
+++ src/main/java/br/com/ccm/api/bugmonitor/model/Bug.java
@@ -1,10 +1,7 @@
 package br.com.ccm.api.bugmonitor.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -13,6 +10,7 @@ import java.util.Set;
 @Entity
 @Table(name = "bugs")
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
