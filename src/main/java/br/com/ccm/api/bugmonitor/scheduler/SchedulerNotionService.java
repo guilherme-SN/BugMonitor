36bba79a2ab1bc1ec40347795bdc14a48542b020
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-04-01 18:46:49 -0300

    feat: add scheduler to run every 5 seconds

--- src/main/java/br/com/ccm/api/bugmonitor/scheduler/SchedulerNotionService.java
+++ src/main/java/br/com/ccm/api/bugmonitor/scheduler/SchedulerNotionService.java
@@ -3,14 +3,16 @@ package br.com.ccm.api.bugmonitor.scheduler;
 import br.com.ccm.api.bugmonitor.service.BugService;
 import br.com.ccm.api.bugmonitor.service.NotionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class SchedulerNotionService {
     private final NotionService notionService;
     private final BugService bugService;
 
+    @Scheduled(cron = "*/5 * * * * *")
     public void retrieveNotionDatabase() {
         notionService.getDatabase().subscribe(bugService::processNotionDatabase);
     }
