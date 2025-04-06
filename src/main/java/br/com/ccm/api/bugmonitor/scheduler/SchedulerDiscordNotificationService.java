7b587ae9fa98718d00640eee52dd21db929b832c
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-04-06 12:25:38 -0300

    feat: create scheduler to send notifications to Discord

---
+++ src/main/java/br/com/ccm/api/bugmonitor/scheduler/SchedulerDiscordNotificationService.java
@@ -0,0 +1,28 @@
+package br.com.ccm.api.bugmonitor.scheduler;
+
+import br.com.ccm.api.bugmonitor.enums.EBugNotificationStatus;
+import br.com.ccm.api.bugmonitor.model.Bug;
+import br.com.ccm.api.bugmonitor.service.BugService;
+import br.com.ccm.api.bugmonitor.service.DiscordNotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class SchedulerDiscordNotificationService {
+    private final BugService bugService;
+    private final DiscordNotificationService discordNotificationService;
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void sendBugsReadyToBeNotified() {
+        Set<Bug> bugsToBeNotified = bugService.getBugsReadyToBeNotified();
+
+        for (Bug bug : bugsToBeNotified) {
+            discordNotificationService.notifyNewBug(bug);
+            bugService.updateNotificationStatus(bug, EBugNotificationStatus.SENT);
+        }
+    }
+}
