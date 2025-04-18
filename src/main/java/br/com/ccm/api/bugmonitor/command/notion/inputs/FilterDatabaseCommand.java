e533c221c68757bf3bbd223a14996a98382fbb7b
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-04-18 15:38:00 -0300

    feat: add sorting to notion responses

--- src/main/java/br/com/ccm/api/bugmonitor/command/notion/inputs/FilterDatabaseCommand.java
+++ src/main/java/br/com/ccm/api/bugmonitor/command/notion/inputs/FilterDatabaseCommand.java
@@ -1,11 +1,14 @@
 package br.com.ccm.api.bugmonitor.command.notion.inputs;
 
 import br.com.ccm.api.bugmonitor.command.notion.inputs.attribute.Filter;
+import br.com.ccm.api.bugmonitor.command.notion.inputs.attribute.Sort;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,6 +16,7 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FilterDatabaseCommand {
     private final Filter filter;
+    private final List<Sort> sorts;
 
     @JsonProperty("start_cursor")
     private String startCursor;
---
+++ src/main/java/br/com/ccm/api/bugmonitor/command/notion/inputs/attribute/Sort.java
@@ -0,0 +1,7 @@
+package br.com.ccm.api.bugmonitor.command.notion.inputs.attribute;
+
+public record Sort(
+        String property,
+        String direction
+) {
+}
