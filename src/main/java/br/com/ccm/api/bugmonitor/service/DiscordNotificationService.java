b0d87658ed41f6372095643c181be206135c1862
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-04-18 18:20:31 -0300

    refactor: delegate message creation to DiscordMessageFactory

--- src/main/java/br/com/ccm/api/bugmonitor/service/DiscordNotificationService.java
+++ src/main/java/br/com/ccm/api/bugmonitor/service/DiscordNotificationService.java
@@ -1,17 +1,13 @@
 package br.com.ccm.api.bugmonitor.service;
 
 import br.com.ccm.api.bugmonitor.command.discord.inputs.SendMessageCommand;
-import br.com.ccm.api.bugmonitor.command.discord.inputs.attribute.*;
 import br.com.ccm.api.bugmonitor.model.Bug;
-import br.com.ccm.api.bugmonitor.model.Customer;
+import br.com.ccm.api.bugmonitor.util.DiscordMessageFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,37 +29,17 @@ public class DiscordNotificationService {
 
     private final WebClient discordWebClient;
 
-    public void notifyNewBug(Bug bug) {
-        SendMessageCommand discordRequest = new SendMessageCommand(
-                "Notificador de Bug - From Notion",
-                List.of(
-                        new Embed(
-                                "\uD83D\uDEA8 [NOVO BUG] \uD83D\uDEA8",
-                                16711680,
-                                List.of(
-                                        new Field("Nome", bug.getName(), false),
-                                        new Field("Clientes Impactados",
-                                                bug.getImpactedCustomers().stream()
-                                                        .map(Customer::getName)
-                                                        .collect(Collectors.joining(", ")),
-                                                false),
-                                        new Field("Prioridade", bug.getPriority(), true),
-                                        new Field("Status", bug.getTaskStatus(), true),
-                                        new Field("Criado por", bug.getCreatedBy().getName(), false)
-                                ),
-                                new Footer("Monitoramento Automático")
-                        )
-                ),
-                List.of(
-                        new MessageComponent(
-                                1,
-                                List.of(
-                                        new Component(2, 5, "Link do card", bug.getUrl())
-                                )
-                        )
-                )
-        );
+    public void sendBugCreatedNotification(Bug bug) {
+        SendMessageCommand discordRequest = DiscordMessageFactory.createBugCreatedMessage(bug);
+        sendNotification(discordRequest, this.generalChannelId);
+    }
 
+    public void sendBugCompletedNotification(Bug bug) {
+        SendMessageCommand discordRequest = DiscordMessageFactory.createBugCompletedMessage(bug);
+        sendNotification(discordRequest, this.generalChannelId);
+    }
+
+    public void sendNotification(SendMessageCommand discordRequest, String channelId) {
         discordWebClient.post()
                 .uri("/webhooks/" + webhookId + "/" + webhookToken)
                 .body(Mono.just(discordRequest), SendMessageCommand.class)
