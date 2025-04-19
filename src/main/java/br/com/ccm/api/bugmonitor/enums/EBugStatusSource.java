6bd99c08adf8894a582c3b9a36ffebe07fcc458a
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-04-19 14:13:20 -0300

    feat: add source name to each enum

--- src/main/java/br/com/ccm/api/bugmonitor/enums/EBugStatusSource.java
+++ src/main/java/br/com/ccm/api/bugmonitor/enums/EBugStatusSource.java
@@ -1,5 +1,15 @@
 package br.com.ccm.api.bugmonitor.enums;
 
 public enum EBugStatusSource {
-    TASK, PRODUCT, QA, BACKEND, FRONTEND
+    TASK("Status da Task"),
+    PRODUCT("Status Produto"),
+    QA("Status QA"),
+    BACKEND("Status Backend"),
+    FRONTEND("Status Frontend");
+
+    public final String sourceName;
+
+    EBugStatusSource(String sourceName) {
+        this.sourceName = sourceName;
+    }
 }
